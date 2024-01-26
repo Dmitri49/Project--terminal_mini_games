fa66075ff7a345fffb6ee3ec29169416ebcc4db8
Dmitri49 <dmitrilissin@gmail.com>
2024-01-26 11:19:25 +0300

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ const play = () => {
     console.log('Welcome to the Brain Games!')
 
     const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
+    console.log('Hello, ' + name + '!')
 }
 
 export {play}
